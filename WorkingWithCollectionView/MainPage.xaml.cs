89c415e7e6a9a74d82ee2d17c81fccd9090235d0
Home <Home@LAPTOP-2E6V6ACM>
2023-03-07 23:03:27 +0200

    update

--- WorkingWithCollectionView/MainPage.xaml.cs
+++ WorkingWithCollectionView/MainPage.xaml.cs
@@ -5,14 +5,17 @@ namespace WorkingWithCollectionView;
 
 public partial class MainPage : ContentPage
 {
+    //אוסף התלמידים
     public ObservableCollection<Student> Students { get; set; }
 
-    #region כדי שהמסך יתעדכן כתוצאה מעדכון הסטודנט עלינו להפעיל את אירוע בכל שינוי ערך שלו. לכן עלינו ליצור אובייקט מגבה
-    private Student _student;
+    #region כדי שהמסך יתעדכן כתוצאה מעדכון נתון עלינו להפעיל את אירוע בכל שינוי ערך שלו. לכן עלינו ליצור שדות מגבה
+   
+    private Student _student;//תלמיד נבחר
     #endregion
 
-    #region בכל שינוי בסטודנט נפעיל את האירוע
+    #region בכל שינוי בשדות נפעיל את האירוע
     public Student Student { get => _student; set { if (_student != value) { _student = value; OnPropertyChanged("Student"); } } }
+    
     #endregion
 
     public MainPage()
@@ -20,11 +23,12 @@ public partial class MainPage : ContentPage
         //נגדיר רשימה ריקה
         Students = new ObservableCollection<Student>();
         //נאתחל את התלמיד הבודד לריק
-        Student = new() { Name = "רוני", Image = "roni.jpg", BirthDate = new DateTime(2006, 2, 19) };
+        Student = new() { Image="dotnet_bot.svg", Name="ברירת מחדל",BirthDate=new DateTime()};
        
         //נקשר את הדף שלנו לאובייקט המכיל את הקוד שלו
         this.BindingContext = this;
         InitializeComponent();
+       
 	}
 
     /// <summary>
@@ -41,8 +45,15 @@ public partial class MainPage : ContentPage
         Students.Add(new() { Name = "Roni", Image = "roni.jpg", BirthDate = new DateTime(2006, 2, 19) });
         //הוספת תלמיד בדרך המלאה
         Students.Add(new Student { Name = "Omer", BirthDate = new DateTime(2006, 2, 9), Image = "omer.jpg" });
+        Student = Students[0];
 
     }
 
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        LoadStudents();
+        OnPropertyChanged(nameof(Students));
+
+    }
 }
 
